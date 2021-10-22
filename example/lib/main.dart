95f3cb9cbf358cf8d79ac4641f8aa35a62aae2b7
yunzhi <313648148@qq.com>
2021-10-22 10:34:31 +0800

    example code adjustment

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'pages/home_page.dart';
+import 'package:z_dialog/z_dialog.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,6 +16,70 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            ZButton(
+              isDark: true,
+              text: 'AlertDialog',
+              onPressed: () async {
+                ZDialog.showAlert(context,
+                    title: 'Flutter',
+                    content:
+                        'Flutter is Google UI toolkit for building beautiful, natively compiled applications for mobile, web, desktop, and embedded devices from a single codebase.',
+                    actions: <Widget>[
+                      ZButton(
+                        isDark: true,
+                        text: '确定',
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      ZButton(
+                        isDark: false,
+                        text: '取消',
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ]);
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ZButton(
+              isDark: true,
+              text: 'BottomDialog',
+              onPressed: () async {
+                ZDialog.showBottomDialog(context,
+                    options: ['Ring', 'Koa', 'Flutter'], onClick: (i, value) {
+                  Navigator.of(context).pop();
+                });
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
